b2f5d7e2b4838682d11eb618fedf9f1488e852ad
Anton Chen <contact@antonchen.ca>
2021-08-07 16:14:16 -0600

    Update commands and bindings

--- dwm/config.h
+++ dwm/config.h
@@ -45,7 +45,8 @@ static const Rule rules[] = {
 	 *	WM_NAME(STRING) = title
 	 */
 	/* class      instance    title       tags mask     isfloating   monitor */
-	{ "Gimp",     NULL,       NULL,       0,            1,           -1 },
+	{ "feh",     NULL,       NULL,       0,            1,           -1 },
+	{ "mpv",     NULL,       NULL,       0,            1,           -1 },
 	// { "Firefox",  NULL,       NULL,       1 << 8,       0,           -1 },
 };
 
@@ -78,51 +79,63 @@ static const Layout layouts[] = {
 static char dmenumon[2] = "0"; /* component of dmenucmd, manipulated in spawn() */
 static const char *dmenucmd[] = { "dmenu_run", "-m", dmenumon, "-fn", dmenufont, "-nb", col_gray1, "-nf", col_gray3, "-sb", col_cyan, "-sf", col_gray4, NULL };
 static const char *termcmd[]  = { "st", NULL };
+static const char *smalltermcmd[]  = { "/home/anton/.local/bin/sst", NULL };
 
 /* media keys */
+static const char *upbright[] = { "/bin/zsh", "/home/anton/.local/bin/backlight_incr", NULL };
+static const char *downbright[] = { "/bin/zsh", "/home/anton/.local/bin/backlight_decr", NULL };
+
 static const char *upvol[]   = { "/usr/bin/amixer", "set", "Master", "5%+",     NULL };
 static const char *downvol[] = { "/usr/bin/amixer", "set", "Master", "5%-",     NULL };
 static const char *mutevol[] = { "/usr/bin/amixer", "set",   "Master", "toggle",  NULL };
 static const char *toggleplay[] = { "/usr/bin/playerctl", "play-pause", NULL };
 static const char *playprev[] = { "/usr/bin/playerctl", "previous", NULL };
 static const char *playnext[] = { "/usr/bin/playerctl", "next", NULL };
-static const char *lockscreen[] = { "/usr/bin/zsh", "~/.scripts/lock.sh", NULL };
+
+/* misc commands */
+static const char *lockscreen[] = { "/usr/bin/physlock", NULL };
+static const char *st_smallfont[] = { "/home/anton/.local/bin/st-smallfont", NULL };
+static const char *exitdwm[] = { "/usr/bin/killall", "xinit", NULL };
 
 static Key keys[] = {
-	/* modifier                     key        function        argument */
-	{ MODKEY,                       XK_d,      spawn,          {.v = dmenucmd } },
-	{ MODKEY|ShiftMask,             XK_Return, spawn,          {.v = termcmd } },
-	{ MODKEY,                       XK_b,      togglebar,      {0} },
-	{ MODKEY,                       XK_j,      focusstack,     {.i = +1 } },
-	{ MODKEY,                       XK_k,      focusstack,     {.i = -1 } },
-	{ MODKEY,                       XK_n,      incnmaster,     {.i = +1 } },
-	{ MODKEY,                       XK_m,      incnmaster,     {.i = -1 } },
-	{ MODKEY,                       XK_h,      setmfact,       {.f = -0.05} },
-	{ MODKEY,                       XK_l,      setmfact,       {.f = +0.05} },
-	{ MODKEY,                       XK_Return, zoom,           {0} },
-	{ MODKEY,                       XK_Tab,    view,           {0} },
-	{ MODKEY|ShiftMask,             XK_q,      killclient,     {0} },
-	{ 0,                       XF86XK_AudioLowerVolume, spawn, {.v = downvol } },
-	{ 0,                       XF86XK_AudioMute, spawn, {.v = mutevol } },
-	{ 0,                       XF86XK_AudioRaiseVolume, spawn, {.v = upvol   } },
-	{ MODKEY,                       XK_space, spawn, {.v = toggleplay   } },
-	{ MODKEY,                       XK_o, spawn, {.v = playprev   } },
-	{ MODKEY,                       XK_p, spawn, {.v = playnext   } },
-	{ MODKEY|ShiftMask,                       XK_l, spawn, {.v = lockscreen   } },
-	{ MODKEY,                       XK_t,      setlayout,      {.v = &layouts[0]} },
-	// { MODKEY,                       XK_f,      setlayout,      {.v = &layouts[1]} },
-	// { MODKEY,                       XK_m,      setlayout,      {.v = &layouts[2]} },
-	{ MODKEY,                       XK_u,      setlayout,      {.v = &layouts[2]} },
-    { MODKEY,                       XK_f,      fullscreen,     {0} },
-	// { MODKEY|ShiftMask,             XK_space,  setlayout,      {0} },
-	{ MODKEY|ShiftMask,             XK_i,  setlayout,      {0} },
-	// { MODKEY,                       XK_space,  togglefloating, {0} },
-	{ MODKEY,                       XK_0,      view,           {.ui = ~0 } },
-	{ MODKEY|ShiftMask,             XK_0,      tag,            {.ui = ~0 } },
-	{ MODKEY,                       XK_comma,  focusmon,       {.i = -1 } },
-	{ MODKEY,                       XK_period, focusmon,       {.i = +1 } },
-	{ MODKEY|ShiftMask,             XK_comma,  tagmon,         {.i = -1 } },
-	{ MODKEY|ShiftMask,             XK_period, tagmon,         {.i = +1 } },
+	/* modifier                     key			    function        argument */
+	{ MODKEY,                       XK_d,      		    spawn,          {.v = dmenucmd } },
+	{ MODKEY|ShiftMask,             XK_Return, 		    spawn,          {.v = termcmd } },
+	{ MODKEY|ControlMask|ShiftMask, XK_Return, 		    spawn,          {.v = smalltermcmd } },
+	{ MODKEY,                       XK_b,      		    togglebar,      {0} },
+	{ MODKEY,                       XK_j,      		    focusstack,     {.i = +1 } },
+	{ MODKEY,                       XK_k,      		    focusstack,     {.i = -1 } },
+	{ MODKEY,                       XK_n,      		    incnmaster,     {.i = +1 } },
+	{ MODKEY,                       XK_m,      		    incnmaster,     {.i = -1 } },
+	{ MODKEY,                       XK_h,      		    setmfact,       {.f = -0.05} },
+	{ MODKEY,                       XK_l,      		    setmfact,       {.f = +0.05} },
+	{ MODKEY,                       XK_Return, 		    zoom,           {0} },
+	{ MODKEY,                       XK_Tab,    		    view,           {0} },
+	{ MODKEY|ShiftMask,             XK_q,      		    killclient,     {0} },
+	{ MODKEY|ControlMask|ShiftMask, XK_q,			    spawn,	    {.v = exitdwm   } },
+	{ MODKEY,                       XK_space,		    spawn,	    {.v = toggleplay   } },
+	{ MODKEY,                       XK_o,			    spawn, 	    {.v = playprev   } },
+	{ MODKEY,                       XK_p, 	    		    spawn, 	    {.v = playnext   } },
+	{ MODKEY|ShiftMask,             XK_l, 	    		    spawn, 	    {.v = lockscreen   } },
+	{ 0,				XF86XK_AudioLowerVolume,    spawn,	    {.v = downvol } },
+	{ 0,                       	XF86XK_AudioMute,	    spawn,	    {.v = mutevol } },
+	{ 0,                       	XF86XK_AudioRaiseVolume,    spawn,	    {.v = upvol   } },
+	{ 0,				XF86XK_MonBrightnessUp,	    spawn,	    {.v = upbright} },
+	{ 0,				XF86XK_MonBrightnessDown,   spawn,	    {.v = downbright} },
+	{ MODKEY,                       XK_t,			    setlayout,      {.v = &layouts[0]} },
+	// { MODKEY,                       XK_f,   		       setlayout,      {.v = &layouts[1]} },
+	// { MODKEY,                       XK_m,   		       setlayout,      {.v = &layouts[2]} },
+	{ MODKEY,                       XK_u,      		    setlayout,      {.v = &layouts[2]} },
+	{ MODKEY,                       XK_f,      		    fullscreen,     {0} },
+	// { MODKEY|ShiftMask,             XK_space,			setlayout,      {0} },
+	{ MODKEY|ShiftMask,             XK_i,			    setlayout,      {0} },
+	// { MODKEY,                       XK_space,		    togglefloating, {0} },
+	{ MODKEY,                       XK_0,			    view,           {.ui = ~0 } },
+	{ MODKEY|ShiftMask,             XK_0,      		    tag,            {.ui = ~0 } },
+	{ MODKEY,                       XK_comma,  		    focusmon,       {.i = -1 } },
+	{ MODKEY,                       XK_period, 		    focusmon,       {.i = +1 } },
+	{ MODKEY|ShiftMask,             XK_comma,  		    tagmon,         {.i = -1 } },
+	{ MODKEY|ShiftMask,             XK_period, 		    tagmon,         {.i = +1 } },
 	TAGKEYS(                        XK_1,                      0)
 	TAGKEYS(                        XK_2,                      1)
 	TAGKEYS(                        XK_3,                      2)
